d8f5a0c74d1d6d7d1aede99bc764cb264b2bb6cf
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2025-10-04 18:09:29 -0700

    Test updates

--- tests/Controllers/PostsControllerTest.php
+++ tests/Controllers/PostsControllerTest.php
@@ -4,16 +4,52 @@ declare(strict_types=1);
 
 namespace Tests\Controllers;
 
+use App\Controllers\PostsController;
+use App\Data\Paginator;
+use App\Data\Post;
+use App\Posts;
+use Carbon\Carbon;
+use Fig\Http\Message\StatusCodeInterface;
+use Illuminate\Support\Collection;
+use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\TestWith;
+use Psr\Http\Message\ResponseInterface;
+use Slim\Psr7\Response;
+use Slim\Views\Twig;
 use Tests\TestCase;
 
+#[CoversClass(PostsController::class)]
 class PostsControllerTest extends TestCase
 {
-    #[Test]
-    public function it_can_do_something(): void
+    #[Test, TestWith([1]), TestWith([2]), TestWith([3])]
+    public function it_returns_the_list_of_posts_for_a_given_page(int $page): void
     {
-        // ...
+        $this->container->set('posts_per_page', $perPage = 4);
 
-        $this->markTestIncomplete();
+        $posts = $this->mock(Posts::class);
+        $posts->expects($this->once())->method('all')->willReturn(
+            $postsCollection = Collection::times(10, fn (int $iteration): Post => new Post(
+                title: sprintf('Test Post %d', $iteration),
+                body: 'Post body...',
+                published: Carbon::now()->subDays($iteration),
+            ))
+        );
+
+        $twig = $this->mock(Twig::class);
+        $twig->expects($this->once())->method('render')->with($testResponse = new Response, 'posts.twig', [
+            'posts' => $postsCollection->forPage($page, $perPage),
+            'pagination' => new Paginator($postsCollection, $perPage, $page),
+        ])->willReturn(
+            $testResponse->withStatus(StatusCodeInterface::STATUS_OK)
+        );
+
+        $response = $this->container->call(PostsController::class, [
+            'response' => $testResponse,
+            'page' => $page,
+        ]);
+
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertSame(StatusCodeInterface::STATUS_OK, $response->getStatusCode());
     }
 }
--- tests/Controllers/TagsControllerTest.php
+++ tests/Controllers/TagsControllerTest.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace Tests\Controllers;
 
-use App\Controllers\TagController;
+use App\Controllers\TagsController;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
-#[CoversClass(TagController::class)]
-class TagControllerTest extends TestCase
+#[CoversClass(TagsController::class)]
+class TagsControllerTest extends TestCase
 {
     #[Test]
     public function it_can_do_something(): void
