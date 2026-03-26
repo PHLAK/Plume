b90b04cdd4c6d4beff4a9f3f3c6c8ee318c9943d
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2026-03-25 21:43:43 -0700

    Another attempt at fixing the analytics script

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,18 +1,16 @@
 import { defineConfig } from 'vitepress'
 
-const analyticsScript = process.env.ENVIRONMENT === 'production' ? [ 'script', {
-    defer: '',
-    src: 'https://analytics.phlak.net/script.js',
-    'data-website-id': '07fa46f2-8a0d-4cc0-b3aa-c7bf9e1b6be7',
-}] : [];
-
 export default defineConfig({
     title: 'Plume Documentation',
     description: 'The official Plume documentation',
 
     head: [
         ['link', { rel: 'icon', href: '/images/plume.svg' }],
-        ...analyticsScript,
+        [ 'script', {
+            defer: '',
+            src: 'https://analytics.phlak.net/script.js',
+            'data-website-id': '07fa46f2-8a0d-4cc0-b3aa-c7bf9e1b6be7',
+        }],
     ],
 
     themeConfig: {
